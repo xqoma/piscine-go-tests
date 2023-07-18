b64e012934ae3b2d67250e4a76028b7fcba4bddd
Dmitry Grigoriev <dmitry.grigoriev@wisercat.net>
2023-07-18 06:39:39 +0300

    Adds the  test

---
+++ go.mod
@@ -0,0 +1,10 @@
+module piscine-go-tests
+
+go 1.20
+
+replace example.com/piscine-go => ../piscine-go
+
+require (
+	example.com/piscine-go v0.0.0-00010101000000-000000000000 // indirect
+	github.com/01-edu/z01 v0.1.0 // indirect
+)
---
+++ tests/ztail_test/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"piscine-go-tests/lib/challenge"
+	"piscine-go-tests/lib/chars"
+	"piscine-go-tests/lib/random"
+)
+
+func main() {
+	file1 := "quest8.txt"
+	file2 := "quest8T.txt"
+	if err := ioutil.WriteFile(file1, []byte(random.Str(chars.Words, 13)+"\n"), os.ModePerm); err != nil {
+		panic(err)
+	}
+	if err := ioutil.WriteFile(file2, []byte(random.Str(chars.Words, 13)+"\n"), os.ModePerm); err != nil {
+		panic(err)
+	}
+	table := [][]string{
+		{"-c", "23", "../../../../piscine-go/ztail/main.go", file1},
+		{"-c", "13", "../../../../piscine-go/ztail/main.go", "fjksdsf", file2},
+		{"-c", "5", file1, file2},
+		{"-c", "1", "fjksdsf"},
+		{"-c", "1", file1, file2},
+	}
+	for _, args := range table {
+		challenge.Program("ztail", args...)
+	}
+}
